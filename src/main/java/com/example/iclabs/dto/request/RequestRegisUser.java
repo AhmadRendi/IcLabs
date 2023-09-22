d1a03e72d455104e3af53afd1264b2307c4da0b4
ahmad rendi <ahmadrendiajah@gmail.com>
2023-09-22 14:07:37 +0800

    ada perubahan

--- src/main/java/com/example/iclabs/dto/request/RequestRegisUser.java
+++ src/main/java/com/example/iclabs/dto/request/RequestRegisUser.java
@@ -1,4 +1,16 @@
 package com.example.iclabs.dto.request;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class RequestRegisUser {
+
+    private String nim;
+
+    private String name;
+
+    private String pass;
+
 }
